5946d0591e869f70f7467eed847aecf7cc2210b9
programmer-anvar <anvartojiboyev733@gmail.com>
2025-02-10 02:34:57 +0500

    add table

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,6 +1,9 @@
 import Layoutss from '@/components/Layout';
+import HomePage1 from '@/pages/homa-page/HomePage-1';
+import HomePage2 from '@/pages/homa-page/HomePage-2';
 import Home from '@/pages/home/Home';
 import Login from '@/pages/login/Login';
+import TotalIncome from '@/pages/total-income/TotalIncome';
 import { createBrowserRouter } from 'react-router-dom';
 
 export const protectedRouter = createBrowserRouter([
@@ -9,6 +12,30 @@ export const protectedRouter = createBrowserRouter([
 		element: (
 			<Layoutss>
 				<Home />
+			</Layoutss>
+		),
+	},
+	{
+		path: '/homePAge-1',
+		element: (
+			<Layoutss>
+				<HomePage1 />
+			</Layoutss>
+		),
+	},
+	{
+		path: '/homePAge-2',
+		element: (
+			<Layoutss>
+				<HomePage2 />
+			</Layoutss>
+		),
+	},
+	{
+		path: '/TotalIncome',
+		element: (
+			<Layoutss>
+				<TotalIncome />
 			</Layoutss>
 		),
 	},
