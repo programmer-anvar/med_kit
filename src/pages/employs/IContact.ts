f0f7f5d1187542f8cf584a2d99a6e3396a47809a
programmer-anvar <anvartojiboyev733@gmail.com>
2025-02-10 20:02:00 +0500

    remove employe

--- src/pages/employs/IContact.ts
+++
@@ -1,7 +0,0 @@
-export interface IContact {
-	id: number;
-	name: string;
-	role: string; // Corresponds to 'Holati' in your screenshot
-	profession: string; // Corresponds to 'Kasbi'
-	phone: string;
-}
